4ee4e68c9d77954220dabdd85c2b60a698305905
Valeriy <goldaboss@yandex.ru>
2023-02-17 15:09:21 +0300

    routing for products and user components in main module

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
+import {ProductsOverviewComponent} from "./components/products-overview/products-overview.component";
+import {UserOverviewComponent} from "./components/user-overview/user-overview.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'products', component: ProductsOverviewComponent},
+  {path: 'user', component: UserOverviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
