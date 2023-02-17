9b08f224509778e4d4230ce389c01436cbdfbdb5
Valeriy <goldaboss@yandex.ru>
2023-02-17 15:32:16 +0300

    add about link in nav (header component) add about & product components in main module add common module in main module

--- src/app/main/main-module.ts
+++ src/app/main/main-module.ts
@@ -2,14 +2,20 @@ import {NgModule} from "@angular/core";
 import {MainRoutingModule} from "./main-routing.module";
 import {MainOverviewComponent} from "./components/main-overview/main-overview.component";
 import {UserOverviewComponent} from "./components/user-overview/user-overview.component";
+import {CommonModule} from "@angular/common";
+import {ProductsOverviewComponent} from "./components/products-overview/products-overview.component";
+import {AboutOverviewComponent} from "./components/about-overview/about-overview.component";
 
 @NgModule({
   declarations: [
     MainOverviewComponent,
-    UserOverviewComponent
+    AboutOverviewComponent,
+    UserOverviewComponent,
+    ProductsOverviewComponent,
   ],
   imports: [
     MainRoutingModule,
+    CommonModule
   ],
   providers: [],
   exports: []
