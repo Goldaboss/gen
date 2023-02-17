ee151eb413a24bb2bad8d5d247676c5bb4d7297b
Valeriy <goldaboss@yandex.ru>
2023-02-17 14:46:07 +0300

    create header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainOverviewComponent} from "./main/components/main-overview/main-overview.component";
+import {AuthOverviewComponent} from "./auth/components/auth-overview/auth-overview.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'main', component: MainOverviewComponent},
+  {path: 'auth', component: AuthOverviewComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/main/main-module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from "@angular/core";
+import {MainRoutingModule} from "./main-routing.module";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MainRoutingModule,
+  ],
+  providers: [],
+  exports: []
+})
+export class MainModule {}
