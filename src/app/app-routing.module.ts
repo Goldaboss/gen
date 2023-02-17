df256d84d236ba490c0c98c37aabb80d9e6556e0
Valeriy <goldaboss@yandex.ru>
2023-02-17 15:34:35 +0300

    create rout for main module create paths for children components (main: about, user, products) add router outlet for main-overview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MainOverviewComponent} from "./main/components/main-overview/main-overview.component";
 import {AuthOverviewComponent} from "./auth/components/auth-overview/auth-overview.component";
+import {MainModule} from "./main/main-module";
 
 const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'},
-  {path: 'main', component: MainOverviewComponent},
+  {path: 'main', loadChildren: () => MainModule},
   {path: 'auth', component: AuthOverviewComponent},
 ];
 
--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,11 +2,18 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {ProductsOverviewComponent} from "./components/products-overview/products-overview.component";
 import {UserOverviewComponent} from "./components/user-overview/user-overview.component";
+import {MainOverviewComponent} from "./components/main-overview/main-overview.component";
+import {AboutOverviewComponent} from "./components/about-overview/about-overview.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: 'main', pathMatch: 'full'},
-  {path: 'products', component: ProductsOverviewComponent},
-  {path: 'user', component: UserOverviewComponent}
+  {
+    path: '', component: MainOverviewComponent, children: [
+      {path: '', redirectTo: 'about', pathMatch: 'full' },
+      {path: 'about', component: AboutOverviewComponent},
+      {path: 'products', component: ProductsOverviewComponent},
+      {path: 'user', component: UserOverviewComponent}
+    ]
+  }
 ];
 
 @NgModule({
