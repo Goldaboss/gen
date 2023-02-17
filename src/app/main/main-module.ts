a8c35b9ba7404b4a6f86284d8dcc3a565aea59d1
Valeriy <goldaboss@yandex.ru>
2023-02-17 15:05:42 +0300

    create user component and products component

--- src/app/main/main-module.ts
+++ src/app/main/main-module.ts
@@ -1,8 +1,13 @@
 import {NgModule} from "@angular/core";
 import {MainRoutingModule} from "./main-routing.module";
+import {MainOverviewComponent} from "./components/main-overview/main-overview.component";
+import {UserOverviewComponent} from "./components/user-overview/user-overview.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainOverviewComponent,
+    UserOverviewComponent
+  ],
   imports: [
     MainRoutingModule,
   ],
