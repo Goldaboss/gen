54d2aeb5df74d3f3bb8f29a4f3ae22b32d1259a6
Valeriy <goldaboss@yandex.ru>
2023-03-13 16:44:38 +0300

    create loading spinner add loading spinner for login component

--- src/app/main/main-module.ts
+++ src/app/main/main-module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+
 import {MainRoutingModule} from "./main-routing.module";
 import {MainOverviewComponent} from "./components/main-overview/main-overview.component";
 import {UserOverviewComponent} from "./components/user-overview/user-overview.component";
-import {CommonModule} from "@angular/common";
 import {ProductsOverviewComponent} from "./components/products-overview/products-overview.component";
 import {AboutOverviewComponent} from "./components/about-overview/about-overview.component";
 
