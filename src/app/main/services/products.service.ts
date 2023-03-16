4149b5221ef5185ea9127d84864261b456cfc1c2
Valeriy <goldaboss@yandex.ru>
2023-03-16 18:17:32 +0300

    changed http on request-builder

--- src/app/main/services/products.service.ts
+++ src/app/main/services/products.service.ts
@@ -1,18 +1,23 @@
 import {Injectable} from "@angular/core";
-import {BehaviorSubject} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject, finalize} from "rxjs";
 import {ProductModel, ProductsResponseModel} from "../models/product.model";
+import {RequestBuilder} from "../../data-access/services/request-builder";
 
 @Injectable()
 export class ProductsService {
 
-  public productData$ = new BehaviorSubject<ProductModel[]>([])
+  public productData$: BehaviorSubject<ProductModel[]> = new BehaviorSubject<ProductModel[]>([])
+  public loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
 
-  constructor(private http: HttpClient) {
+  constructor(private request: RequestBuilder) {
   }
 
   public getList(): void {
-    this.http.get<ProductsResponseModel>('https://dummyjson.com/auth/products')
+    this.loading$.next(true)
+    this.request.to(`auth/products`).get<ProductsResponseModel>()
+      .pipe(
+        finalize(()=> this.loading$.next(false))
+      )
       .subscribe((response) => {
         this.productData$.next(response.products)
       })
