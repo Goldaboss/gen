4cf144a885a17423e53e77245446d9b192249330
Valeriy <goldaboss@yandex.ru>
2023-03-17 17:32:36 +0300

    fix titlecase

--- src/app/main/components/products-overview/lib/state/products.actions.ts
+++ src/app/main/components/products-overview/lib/state/products.actions.ts
@@ -1,4 +1,4 @@
-import {ProductStateParamsModel} from "../../models/Product.model";
+import {ProductStateParamsModel} from "../../models/product.model";
 
 export class GetProductsList {
   static readonly type = '[Products Api] Get Products List';
