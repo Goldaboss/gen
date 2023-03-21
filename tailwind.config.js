077a83a1892372431710c545c18d01aa67ef2bf8
Valeriy <goldaboss@yandex.ru>
2023-03-21 17:58:54 +0300

    tailwind config add screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px'
+      }
+    },
   },
   plugins: [],
 }
