03eef6ef5207ac871e126cb133c0ed76bd81f7a9
Valeriy <goldaboss@yandex.ru>
2023-04-27 11:46:18 +0300

    create product-detail-api service

---
+++ src/app/product-detail/service/product-detail-api.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {RequestBuilder} from "../../modules/data-access/services/request-builder";
+import {Observable} from "rxjs";
+import {ProductModel} from "../../products/models/product.model";
+
+@Injectable()
+export class ProductDetailApiService {
+
+  constructor(private request: RequestBuilder) {
+  }
+
+  public getProductDetail(id: number): Observable<ProductModel> {
+    return this.fetch(`product/${id}`).get<ProductModel>()
+  }
+
+  private fetch(url: string) {
+    return this.request.to(`auth/${url}`)
+  }
+}
