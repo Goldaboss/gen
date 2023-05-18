35e935302288e4ad65821935a719837652fac38b
Valeriy <goldaboss@yandex.ru>
2023-05-18 10:48:06 +0300

    fex text About change " on '

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatCardModule } from "@angular/material/card";
 
-import { UserRoutingModule } from './user-routing.module';
-import { UserOverviewComponent } from './components/user-overview/user-overview.component';
-import { UserDetailsComponent } from './components/user-details/user-details.component';
-import { UserStoreModule } from './store/user-store.module';
-import { SharedModule } from '../modules/shared/shared.module';
+import { UserRoutingModule } from "./user-routing.module";
+import { UserOverviewComponent } from "./components/user-overview/user-overview.component";
+import { UserDetailsComponent } from "./components/user-details/user-details.component";
+import { UserStoreModule } from "./store/user-store.module";
+import { SharedModule } from "../modules/shared/shared.module";
 
 @NgModule({
   declarations: [UserOverviewComponent, UserDetailsComponent],
